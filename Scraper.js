8495cdb20f850cc4e09bc2756187546280f8c6dd
Redtius <redtius@gmail.com>
2023-07-29 19:23:08 +0100

    Fixed tons of bugs

--- Scraper.js
+++ Scraper.js
@@ -1,73 +1,104 @@
-'use strict';
-const puppeteer = require('puppeteer');
-const fs = require('fs');
-const { url } = require('inspector');
+"use strict";
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+const { URLSearchParams } = require("url");
 
+function extractActualLink(encodedUrl) {
+  // Step 1: Find the start and end positions of the encoded part
+  const startPos = encodedUrl.indexOf('/dp/') + 4;
+  const endPos = encodedUrl.indexOf('/ref=');
 
+  // Step 2: Extract the encoded part
+  const encodedPart = encodedUrl.slice(startPos, endPos);
 
+  // Step 3: Reconstruct the original URL
+  const originalUrl = `https://www.amazon.com/dp/${encodedPart}${encodedUrl.slice(endPos)}`;
+
+  return originalUrl;
+}
 // this a Scraper you give it a link in Amazon and it scrapes all the products in it
 class Scraper {
   constructor(browser, url) {
     this.url = url;
     this.browser = browser;
-  }
-  delay(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
+    this.dataFile = new CsvFile(
+      'Products',
+      "Image,Last Month Sales,Price,Name,Number Of Reviews,Rating,Launch Date,Best Seller Rank,Category,Brand"
+    );
   }
   async openPage() {
-    {
       this.page = await this.browser.newPage();
       await this.page.goto(this.url);
-    }
+  }
+
+  async openAnotherPage(url) {
+        const deUrl=extractActualLink(url);
+        const page = await this.browser.newPage();
+        await page.goto(deUrl);
+  
+        return page;
   }
 
   async scrapeData() {
+    let item = {};
     try {
       let isDisabled =
         (await this.page.$(
           "span.s-pagination-item.s-pagination-next.s-pagination-disabled"
         )) === null;
-      const productHandles = await this.page.$$("div.s-result-item");
-      const dataFile = new CsvFile(Products,'Image,Last Month Sales,')
       while (isDisabled) {
-        //await delay(2000)
+        const productHandles = await this.page.$$("div.s-result-item");
         isDisabled =
-          (await page.$(
+          (await this.page.$(
             "span.s-pagination-item.s-pagination-next.s-pagination-disabled"
           )) === null;
-        for (const productHandle of productHandles) {
-          let item = {};
+          for (let i = 0;i<productHandles.length;i++) {
+            const productHandle=productHandles[i];
           //Image Extraction
           try {
             const Image = await this.page.evaluate(
               (el) =>
                 el
                   .querySelector(
-                    " div.s-product-image-container > span > a > div > img"
+                    '.s-image'
                   )
                   .getAttribute("src"),
               productHandle
             );
-            dataFile.Write(Image,true);
-          } catch (error) {}
+            item.Image=Image;
+          } catch (error) {
+            item.Image='Unknown';
+          }
           // Last Month Sales
           try {
             //const
-          } catch (error) {}
-
-          await this.page.click(
-            "a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal"
-          );
-          pageScraper = new ProductScraper(this.page);
-          await pageScraper.scrapeProduct(); // it will automatically comeback to the page
+            item.LMS='Unknown';
+          } catch (error) {
+            item.LMS='Unknown';
+          }
+          
+          try {
+            const link = await this.page.evaluate(
+              (el) =>el.querySelector(
+                    '.a-link-normal.s-underline-text.s-underline-link-text.s-link-style.a-text-normal'
+                  )
+                  .getAttribute("href"),
+              productHandle
+            );
+            const page = await this.openAnotherPage(link);
+            const pageScraper = new ProductScraper(page, this.dataFile);
+            const details = await pageScraper.scrapeProduct(); // it will automatically come back to the page
+            //this.dataFile.Write(Image, true);
+          } catch (error) {
+            //this.dataFile.Write('Not Found', true);
+          }
         }
-
         if (isDisabled) {
-          await page.waitForSelector(
+          await this.page.waitForSelector(
             "a.s-pagination-item.s-pagination-next.s-pagination-button.s-pagination-separator",
             { visible: true }
           );
-          await page.click(
+          await this.page.click(
             "a.s-pagination-item.s-pagination-next.s-pagination-button.s-pagination-separator"
           );
         } else {
@@ -85,166 +116,175 @@ class Scraper {
 }
 
 //<-------------Product Scraper--------------->
-class ProductScraper{
-  constructor(page){
-    this.page=page;
+class ProductScraper {
+  constructor(page, dataFile) {
+    this.page = page;
+    this.dataFile = dataFile;
   }
-  async scrapeProduct(){
+  async scrapeProduct() {
+    let details = {};
     // price
-    try{
+    try {
       const price = await this.page.evaluate(() => {
-        const element = document.querySelector('span.a-price > span.a-offscreen');
-        return element ? element.textContent : 'Not found';
+        const element = document.querySelector(
+          "span.a-price > span.a-offscreen"
+        );
+        return element ? element.textContent : "Not found";
       });
-      console.log(price);
+      details.price=price;
+    } catch (error) {
+      details.price='Unknown';
     }
-    catch(error){}
-    //image
-    try{
-      const image = await this.page.evaluate(() => {
-        const element = document.querySelector('#landingImage');
-        return element ? element.getAttribute('src') : 'Not found';
-      });
-      console.log(image);
-    }
-    catch(error){}
-    //name
-    try{
+    try {
       let nameTemp = await this.page.evaluate(() => {
-        const element = document.querySelector('#productTitle');
-        return element ? element.textContent : 'Not found';
+        const element = document.querySelector("#productTitle");
+        return element ? element.textContent : "Not found";
       });
       const name = nameTemp.ProductName.replace(/[\n,"']/g, " ").trim();
-      console.log(name);
+      details.name=name;
+    } catch (error) {
+      details.name='Unknown';
     }
-    catch(error){}
     // Number Of Reviews
-    try{
+    try {
       let ratingsTemp = await this.page.evaluate(() => {
-        const element = document.querySelector('#acrCustomerReviewText');
-        return element ? element.textContent : 'Not found';
-      })
+        const element = document.querySelector("#acrCustomerReviewText");
+        return element ? element.textContent : "Not found";
+      });
       const ratings = parseInt(ratingsTemp.replace(/[,]/g, ""));
-      console.log(ratings);
-    }
-    catch(error){
-      console.log('error');
+      details.ratings = ratings;
+    } catch (error) {
+      details.ratings = 'Unknown'
     }
     // Rating
-    try{
-      const rating = parseFloat(await this.page.evaluate(() => {
-        const element = document.querySelector('#acrPopover > span.a-declarative > a > span');
-        return element ? element.textContent : 'Not found';
-      }));
-      console.log(rating);
-    }
-    catch(error){
-      console.log('error');
+    try {
+      const rating = parseFloat(
+        await this.page.evaluate(() => {
+          const element = document.querySelector(
+            "#acrPopover > span.a-declarative > a > span"
+          );
+          return element ? element.textContent : "Not found";
+        })
+      );
+      details.rating=rating;
+    } catch (error) {
+      details.rating='unknown';
     }
     //Launch Date
-    try{
+    try {
       const launchDate = await this.page.evaluate(() => {
-        const element = document.querySelector('#detailBullets_feature_div .detail-bullet-list');
-        let launchDate = 'Not Found';
+        const element = document.querySelector(
+          "#detailBullets_feature_div .detail-bullet-list"
+        );
+        let launchDate = "Not Found";
         if (!element) return null;
-        const listItems = element.querySelectorAll('li');
-        listItems.forEach((item)=>{
+        const listItems = element.querySelectorAll("li");
+        listItems.forEach((item) => {
           const text = item.textContent.trim();
-          if(text.includes('Date First Available')){
-            launchDate = text.slice(27).replace(/[,]/g, "-").replace(/[ :\n]/g,"").trim();
+          if (text.includes("Date First Available")) {
+            launchDate = text
+              .slice(27)
+              .replace(/[,]/g, "-")
+              .replace(/[ :\n]/g, "")
+              .trim();
           }
         });
         return launchDate;
       });
-      console.log(launchDate);
-    }
-    catch(error){
-      console.log(error);
+      details.launchDate=launchDate;
+    } catch (error) {
+      details.launchDate='Unknown';
     }
     //BSR (best seller ranking)
-    try{
+    try {
       const bsr = await this.page.evaluate(() => {
-        const element = document.querySelector('#detailBulletsWrapper_feature_div > ul:nth-child(4) > li > span');
-        return element ? element.textContent.slice(27).trim() : 'Not found';
-      })
-      console.log(bsr);
+        const element = document.querySelector(
+          "#detailBulletsWrapper_feature_div > ul:nth-child(4) > li > span"
+        );
+        return element ? element.textContent.slice(27).trim() : "Not found";
+      });
+      details.bsr=bsr;
+    } catch (error) {
+      details.bsr='Unknown';
     }
-    catch(error){
-      console.log('error');
-    }
-    //category 
-    try{
+    //category
+    try {
       const category = await this.page.evaluate(() => {
-        const element = document.querySelector('#wayfinding-breadcrumbs_container');
-        return element ? element.textContent.replace(/[\n, ]/g, "").trim() : 'Not found';
-      })
-      console.log(category);
-    }
-    catch(error){
-      console.log('error');
+        const element = document.querySelector(
+          "#wayfinding-breadcrumbs_container"
+        );
+        return element
+          ? element.textContent.replace(/[\n, ]/g, "").trim()
+          : "Not found";
+      });
+      details.category=category;
+    } catch (error) {
+      details.category="Unknown";
     }
     //Brand
-    try{
+    try {
       let brandString = await this.page.evaluate(() => {
-        const element = document.querySelector('#bylineInfo');
-        return element ? element.textContent.trim() : 'Not found';
+        const element = document.querySelector("#bylineInfo");
+        return element ? element.textContent.trim() : "Not found";
       });
-      const brand = brandString.slice(0,1) === "V"? brandString.slice(10) : brandString.slice(7);
-      
-      console.log(brand);
+      const brand =
+        brandString.slice(0, 1) === "V"
+          ? brandString.slice(10)
+          : brandString.slice(7);
+
+      details.brand=brand;
+    } catch (error) {
+      details.brand="Unknown";
     }
-    catch(error){
-      console.log('error');
-    }
-    await this.previousPage();
+    await this.page.close();
   }
-  async previousPage(){
+  async previousPage() {
     await this.page.goBack();
-  } 
+
+    await this.page.waitForNavigation();
+  }
 }
 //<------------------------------------------->
 
-(async ()=>{
+(async () => {
   const browser = await puppeteer.launch({
-    headless: 'new',
+    headless: false,
     defaultViewport: false,
     userDataDir: "./tmp",
   });
-  
-  const myScraper= new Scraper(browser,'https://www.amazon.com/dp/B097CV6KBQ/ref=syn_sd_onsite_desktop_0?ie=UTF8&psc=1&pf_rd_p=78171839-c733-4857-996f-d6b4a32913ec&pf_rd_r=WHRGY40SKS4JYMYA3J38&pd_rd_wg=lGSbe&pd_rd_w=40dhA&pd_rd_r=a72675a9-60df-454e-ba2f-b14ec5bde4b8')
+
+  const myScraper = new Scraper(
+    browser,
+    "https://www.amazon.com/s?k=i+love+mom+Tshirt&crid=3NJOJ02SWWGKG&sprefix=i+love+mom+tshir%2Caps%2C239&ref=nb_sb_noss_2"
+  );
   await myScraper.openPage();
   await myScraper.scrapeData();
-  await myScraper.closePage();
-  
+
   await browser.close();
 })();
 
-
 //---------->CSV Writer<--------------
 
-class CsvFile{
-  constructor(fileName,columns){
-    this.fileName=`${fileName}.csv`;
-    this.creationDate=new Date();
-    fs.writeFile(this.fileName,columns,'utf-8',(err)=>{
-      if(err){
-        console.error('Failed to Create CSV :',err);
+class CsvFile {
+  constructor(fileName, columns) {
+    this.fileName = `${fileName}.csv`;
+    this.creationDate = new Date();
+    fs.writeFile(this.fileName, columns, "utf-8", (err) => {
+      if (err) {
+        console.error("Failed to Create CSV :", err);
       }
     });
-
   }
 
-  Write(content,isNewLine){
-    if(!isNewLine){
-      fs.appendFile('products.csv', `,${content}`, (err) => {
+  Write(content, isNewLine) {
+    if (!isNewLine) {
+      fs.appendFile(this.fileName, `,${content}`, (err) => {
         if (err) throw err;
-      })
-    }
-    else{
-      fs.appendFile('products.csv', `\n${content}`, (err) => {
+      });
+    } else {
+      fs.appendFile(this.fileName, '\n'+`${content}`, (err) => {
         if (err) throw err;
-      })
+      });
     }
   }
-
 }
